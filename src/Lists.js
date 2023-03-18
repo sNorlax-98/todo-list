6d3dd1b62335d8ed30a93fc1388b7ddc514f8ba9
snorlax-98m <Anupamdas19980@gmail.com>
2023-03-18 10:24:14 +0530

    added checkboxes

--- src/Lists.js
+++ src/Lists.js
@@ -1,16 +1,20 @@
-import {  ListItem } from "@mui/material";
+import {  Checkbox, ListItem } from "@mui/material";
 import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 export default function Lists(props) {
-    
+    const [tick,setTick] = React.useState(false)
+    function toggleClick(event){
+      setTick(event.target.checked)
+    }
   return (
     <>
-      <ListItem sx={{color:"#effbfe" }} className="list-items" >
+      <ListItem sx={{color:"#effbfe", }} className="list-items" >
+        <Checkbox checked={tick} onChange={toggleClick} />
         <DeleteIcon 
           onClick={() => {
             props.delFunc(props.id);
           }}
-        ></DeleteIcon>&nbsp;
+        ></DeleteIcon>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         {props.text}
         
       </ListItem>
